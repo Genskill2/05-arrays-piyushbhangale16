88bcbb512b2a93d66643854adb8d09ddc4b1f897
Piyush Bhangale <62214235+piyushbhangale16@users.noreply.github.com>
2021-06-18 12:27:39 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -35,19 +35,50 @@ float average(int arr[], int n) {
 
 //mode
 int mode(int arr[], int n) {
-  int idx = INT_MIN, a[n] = {0}, ans = 0;
-  for(int i=0; i<n; i++) {
-    a[arr[i]]++;
-  }
-  
-  for(int i=0; i<n; i++) {
-    if(a[i] > idx) {
-      idx = a[i];
-      ans = i;
-    }
-  }
-  
-  return ans;
+  int ans,maxCount=INT_MIN;
+
+ 	for(int i=0; i<n; i++)
+ 	{
+ 		int count=0;
+ 		for(int j=0; j<n; j++)
+ 		{
+ 			if(arr[j] == arr[i])
+ 			count++;
+ 		}
+ 		if(count > maxCount)
+ 		{
+ 			maxCount = count;
+ 			ans = arr[i];
+ 		}
+ 	}
+ 	return maxValue;
 }
 
+//prime factor
+int isPrime(int n) {
+    for(int i=2; i<=n/2; i++) {
+        if(n == 2)
+            return 1;
+        if(n%i == 0)
+            return 0;
+    }
+    return 1;
+} 
 
+int factors(int n, int arr[]) {
+    int ans = 0; 
+    for(int i=2; i<=n; i++) {
+        while(n%i == 0) {
+            if(n == 0) {
+                break;
+            }
+            if(isPrime(i) == 1) {
+                arr[ans] = i;
+                ans++;
+            }
+            n /= i;
+        }
+    }
+
+    return ans;
+}
