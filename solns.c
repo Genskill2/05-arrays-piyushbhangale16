c6f12ada15005da2ad97f72299650d5002f1f8f9
Piyush Bhangale <62214235+piyushbhangale16@users.noreply.github.com>
2021-06-18 09:11:34 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,25 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include <limits.h>
+
+// max
+int max(int arr[], int n) { 
+  int ans = INT_MIN;
+  for(int i=0; i<n; i++) {
+    if(arr[i] > ans)
+      ans = arr[i];
+  }
+  return ans;
+}
+
+//min
+int min(int arr[], int n) {
+  int ans = INT_MAX;
+  for(int i=0; i<n; i++) {
+    if(arr[i] < ans)
+      ans = arr[i];
+  }
+  return ans;
+}
+
+
