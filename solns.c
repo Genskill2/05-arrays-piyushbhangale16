a2338a0ca7f1634d0d1009d6eb4fea8df46a697f
Piyush Bhangale <62214235+piyushbhangale16@users.noreply.github.com>
2021-06-18 09:26:17 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -22,4 +22,32 @@ int min(int arr[], int n) {
   return ans;
 }
 
+//average
+float average(int arr[], int n) {
+  int sum = 0, ans;
+  for(int i=0; i<n; i++) {
+    sum += arr[i];
+  }
+  ans = (float)sum/n;
+  
+  return ans;
+}
+
+//mode
+int mode(int arr[], int n) {
+  int idx = INT_MIN, a[n] = {0}, ans = 0;
+  for(int i=0; i<n; i++) {
+    a[arr[i]]++;
+  }
+  
+  for(int i=0; i<n; i++) {
+    if(a[i] > idx) {
+      idx = a[i];
+      ans = i;
+    }
+  }
+  
+  return ans;
+}
+
 
